e85e4a605e389e00a38bf997fa40b216568bf5c4
xiaoqi <2696299984@qq.com>
2026-07-16 09:36:14 +0800

    Allow SakuraFrp hosts for flash wiki

--- 猎人游戏闪光模式Wiki工程/vite.config.js
+++ 猎人游戏闪光模式Wiki工程/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 
+const tunnelHosts = ['frp-ice.com', '.frp-ice.com'];
+
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1200
   },
   server: {
     host: '0.0.0.0',
-    port: 4174
+    port: 4174,
+    allowedHosts: tunnelHosts
   },
   preview: {
     host: '0.0.0.0',
-    port: 4175
+    port: 4175,
+    allowedHosts: tunnelHosts
   }
 });
